2cb8b7d0248147534ae32a6805caa81db3610804
Yu <3167322194@qq.com>
2024-10-23 01:33:35 +0800

    update models and add functions to create robot.

--- app/models.py
+++ app/models.py
@@ -46,6 +46,7 @@ class User(db.Model):
     id = db.Column(GUID(), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
+    admin = db.Column()
     settings = db.Column(JSON, nullable=True)
     current = db.Column(db.Integer, nullable=False, default=0)  # money
     comments = db.Column(JSON, nullable=True)
@@ -59,11 +60,15 @@ class User(db.Model):
 
 class Bot(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(GUID(), db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String, nullable=False, unique=True)
     url = db.Column(db.String, nullable=False)
-    usage_limit = db.Column(db.Integer, nullable=False)
+    base_model = db.Column(db.String,nullable=False)
+    quota = db.Column(db.Integer, nullable=True)
     price = db.Column(db.Integer, nullable=False)
     prompts = db.Column(JSON, nullable=False)
+    icon = db.Column(db.String, nullable=False)
+    knowledge_base = db.Column(db.String, nullable=True)
 
 
 class Session(db.Model):
--- app/routes/robots.py
+++ app/routes/robots.py
@@ -1,5 +1,70 @@
-from flask import Blueprint, request, jsonify
+from flask import request, Blueprint, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from app.models import db, Bot
+from app.routes.auth import get_user
 
 robots = Blueprint('robots', __name__)
+headers = {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache',
+    'X-Accel-Buffering': 'no',
+}
+
+@robots.route('/robot', methods=['GET'])
+def robot_list():
+    token = request.headers.get('Authorization').split()[1]
+    user = get_user(token)
+
+    if user is None:
+        return jsonify({'msg': 'Invalid Credential'}), 401
+    bots_data = Bot.query.all()
+    try:
+        return jsonify([{
+            #Todo
+            'id': bot.id,
+            'name': bot.name,
+            'url': bot.url,
+            'usage_limit': bot.usage_limit,
+            'price': bot.price,
+            'prompts': bot.prompts
+        } for bot in bots_data])
+    except KeyError:
+        return jsonify({'msg': 'Missing required fields'}), 400
 
 
+@robots.route('/robot/new', methods=['POST'])
+
+def create_robot():
+    """
+    create a new bot
+    """
+    token = request.headers.get('Authorization').split()[1]
+    user = get_user(token)
+    if user is None:
+        return jsonify({'msg': 'Invalid Credential'}), 401
+    try:
+        new_bot = Bot(user_id=user.id,
+                      name=request.form['name'],
+                      url='1',
+                      base_model=request.form['base_model_id'],
+                      prompts=request.form['system_prompt'],
+                      price=request.form['price'],
+                      icon=request.form['icon'],
+                      quota=request.form.get('quota'),
+                      knowledge_base=request.form.get('knowledge_base')
+                      )
+    except KeyError:
+        return jsonify({'msg': 'Missing required fields'}), 400
+    db.session.add(new_bot)
+    db.session.commit()
+    return jsonify({'robotid': new_bot.id,
+                    'robot_name': new_bot.name,
+                    'base_model': new_bot.base_model,
+                    'system_prompt': new_bot.prompts,
+                    'creator': new_bot.user_id,
+                    'icon': new_bot.icon,
+                    'knowledge_bale': new_bot.knowledge_base,
+                    'price': new_bot.price,
+                    'quota': new_bot.quota,
+                    'population': None,
+                    'rate': None})
