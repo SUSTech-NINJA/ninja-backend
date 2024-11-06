7c9fdf6508fb5c115fafd1b435e32f40dd2073ee
Yu <3167322194@qq.com>
2024-11-06 22:58:37 +0800

    add function in shopping.py

--- app/routes/shopping.py
+++ app/routes/shopping.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, request, jsonify
+from app.models import db, User
+from app.routes.auth import get_user
 
 shopping = Blueprint('shopping', __name__)
 
 
+@shopping.route('/shop/current', methods=['GET'])
+def get_user_balance():
+    token = request.headers.get('Authorization').split()[1]
+    user = get_user(token)
+    if user is None:
+        return jsonify({'msg': 'Invalid Credential'}), 401
+    return jsonify({'current': user.current}), 200
 
 
-
+@shopping.route('/shop/buy_package', methods=['POST'])
+def but_package():
+    token = request.headers.get('Authorization').split()[1]
+    user = get_user(token)
+    if user is None:
+        return jsonify({'msg': 'Invalid Credential'}), 401
+    user.current = request.form['result']
+    db.session.commit()
+    return jsonify({'result': user.current}), 200
