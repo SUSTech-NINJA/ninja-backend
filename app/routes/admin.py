ca17ffed130cf230be7a31473c11f12710f82908
Zedd <126555263+we-are-Zed@users.noreply.github.com>
2024-12-03 22:01:07 +0800

    fix: rewarding and exports

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,5 +1,3 @@
-import random
-
 from flask import Blueprint, request, jsonify
 from sqlalchemy import func
 import xlwt
@@ -11,6 +9,7 @@ from app.models import Bot, Comment, db, User, Bill
 from app.routes.auth import get_user
 
 admin = Blueprint('admin', __name__)
+
 
 @admin.route('/admin/robot', methods=['GET'])
 def default_robot_list():
@@ -36,8 +35,9 @@ def default_robot_list():
     except KeyError:
         return jsonify({'msg': 'Missing required fields'}), 400
 
+
 @admin.route('/admin/robot/add', methods=['POST'])
-def create_default_robot(): # create a new bot
+def create_default_robot():  # create a new bot
     token = request.headers.get('Authorization').split()[1]
     user = get_user(token)
     if user is None:
@@ -58,7 +58,7 @@ def create_default_robot(): # create a new bot
                       is_default=True,
                       rate=None,
                       time=datetime.now()
-                    )
+                      )
     except KeyError:
         return jsonify({'msg': 'Missing required fields'}), 400
     db.session.add(new_bot)
@@ -76,11 +76,12 @@ def create_default_robot(): # create a new bot
                     'rate': 0,
                     'time': new_bot.time})
 
+
 @admin.route('/admin/robot/update/<base_model_id>', methods=['POST'])
 def update_robot(base_model_id):
     token = request.headers.get('Authorization').split()[1]
     user = get_user(token)
-    print(token,user,base_model_id,request.headers.get('Authorization'))
+    print(token, user, base_model_id, request.headers.get('Authorization'))
     if user is None:
         return jsonify({'msg': 'Invalid Credential'}), 401
     bot = Bot.query.filter_by(base_model=base_model_id).first()
@@ -121,6 +122,7 @@ def update_robot(base_model_id):
                     'time': bot.time
                     })
 
+
 @admin.route('/admin/robot/update/<base_model_id>', methods=['DELETE'])
 def delete_robot(base_model_id):
     token = request.headers.get('Authorization').split()[1]
@@ -141,7 +143,8 @@ def delete_robot(base_model_id):
 
     db.session.commit()
     return jsonify({'msg': 'Success'})
- 
+
+
 @admin.route('/admin/export/comment/<robot_id>', methods=['GET'])
 def export_comment(robot_id):
     token = request.headers.get('Authorization').split()[1]
@@ -149,15 +152,12 @@ def export_comment(robot_id):
     if user is None:
         return jsonify({'msg': 'Invalid Credential'}), 401
 
-    r = str(random.uniform(1, 10))
-    print(robot_id)
     comments = Comment.query.filter_by(bot_id=robot_id).all()
-    print(comments)
 
     workbook = xlwt.Workbook()
     sheet = workbook.add_sheet('Comments')
 
-    headers = ['ID', 'User ID', 'User Name', 'Bot ID', 'Score', 'Time']
+    headers = ['ID', 'User ID', 'User Name', 'Bot ID', 'Content', 'Score', 'Time']
     for col, header in enumerate(headers):
         sheet.write(0, col, header)
 
@@ -166,16 +166,18 @@ def export_comment(robot_id):
         sheet.write(row, 1, str(comment.user_id))
         sheet.write(row, 2, comment.user_name)
         sheet.write(row, 3, comment.bot_id)
-        sheet.write(row, 4, comment.score)
-        sheet.write(row, 5, comment.time.strftime('%Y-%m-%d %H:%M:%S'))
+        sheet.write(row, 4, comment.content or '')
+        sheet.write(row, 5, comment.score)
+        sheet.write(row, 6, comment.time.strftime('%Y-%m-%d %H:%M:%S'))
 
     # save
     time_str = str(datetime.now())
-    workbook.save('comments-'+ r+'.xls')
-    with open('comments-'+r+ '.xls', 'rb') as f:
+    workbook.save('comments-' + time_str + '.xls')
+    with open('comments-' + time_str + '.xls', 'rb') as f:
         data = f.read()
-        # os.remove('comments-'+r+'.xls')
+        os.remove('comments-' + time_str + '.xls')
     return data, 200
+
 
 @admin.route('/admin/export/summary', methods=['GET'])
 def export_summary():
@@ -186,26 +188,27 @@ def export_summary():
 
     # --------- 用户信息工作表 ---------
     user_sheet = workbook.add_sheet('Users')
-    user_headers = ['ID', 'Username', 'Admin',  'Current', 'Rate']
+    user_headers = ['ID', 'Username', 'Admin', 'Current', 'Rate']
     for col, header in enumerate(user_headers):
         user_sheet.write(0, col, header)
-    
+
     for row, user in enumerate(users, start=1):
         user_sheet.write(row, 0, str(user.id))
         user_sheet.write(row, 1, user.username)
         user_sheet.write(row, 2, user.admin)
         user_sheet.write(row, 3, str(user.current))
         user_sheet.write(row, 4, str(user.rate))
-    
+
     total_users = len(users)
     user_sheet.write(len(users) + 1, 0, f'Number of User：{total_users}')
 
     # --------- 机器人信息工作表 ---------
     bot_sheet = workbook.add_sheet('Bots')
-    bot_headers = ['ID', 'User ID', 'Name', 'Base Model', 'Quota', 'Price', 'Prompts', 'Knowledge Base', 'Is Default', 'Rate']
+    bot_headers = ['ID', 'User ID', 'Name', 'Base Model', 'Quota', 'Price', 'Prompts', 'Knowledge Base', 'Is Default',
+                   'Rate']
     for col, header in enumerate(bot_headers):
         bot_sheet.write(0, col, header)
-    
+
     for row, bot in enumerate(bots, start=1):
         try:
             average_score = db.session.query(func.avg(Comment.score)).filter(Comment.bot_id == bot.id).scalar()
@@ -221,7 +224,7 @@ def export_summary():
         bot_sheet.write(row, 7, bot.knowledge_base)
         bot_sheet.write(row, 8, bot.is_default)
         bot_sheet.write(row, 9, str(average_score))
-    
+
     total_bots = len(bots)
     bot_sheet.write(len(bots) + 1, 0, f'Number of Bot：{total_bots}')
 
@@ -230,24 +233,24 @@ def export_summary():
     bill_headers = ['ID', 'User ID', 'Bill']
     for col, header in enumerate(bill_headers):
         bill_sheet.write(0, col, header)
-    
+
     total_margin = 0
     for row, bill in enumerate(bills, start=1):
-        bill_sheet.write(row, 0, str(bill.id))
+        bill_sheet.write(row, 0, bill.id)
         bill_sheet.write(row, 1, str(bill.user_id))
         bill_sheet.write(row, 2, str(bill.bill))
         total_margin += bill.bill
+
     bill_sheet.write(len(bills) + 1, 0, f'Total Margin：{total_margin}')
 
     time_str = str(datetime.now())
-    workbook.save('summary-'+ time_str +'.xls')
-    with open('summary-'+ time_str +'.xls', 'rb') as f:
+    workbook.save('summary-' + time_str + '.xls')
+    with open('summary-' + time_str + '.xls', 'rb') as f:
         data = f.read()
-        os.remove('summary-'+ time_str +'.xls')
+        os.remove('summary-' + time_str + '.xls')
     return data, 200
 
 
-    
-    
-    
-    
+
+
+
